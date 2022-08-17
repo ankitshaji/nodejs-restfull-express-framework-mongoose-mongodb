74cf6fe8d5421cb07fb8f603df9f9eafa789fbb3
Ankit Shaji <ankitshaji@gmail.com>
2022-08-17 18:56:50 +1200

    nodeJS runtime app connects to mogod + creates db

--- index.js
+++ index.js
@@ -2,12 +2,45 @@
 const express = require("express"); //FunctionObject //express module
 const path = require("path"); //pathObject //path module
 const app = express(); //AppObject
+//mongoose ODM - has callback but also supports promises-ie returns promiseObject (pending,undefined) to -resove(value)(fullfulled,value) or reject(errorMessage)(rejected,errorMessage)
+const mongoose = require("mongoose"); //mongooseObject //mongoose module
+
+// *******************************************
+// CONNECT - nodeJS runtime app connects to default mogod server port + creates db
+// *******************************************
+//mongooseObject.method(url/defaultPortNo/databaseToUse,optionsObject-notNeeded) //returns promiseObject pending
+mongoose
+  .connect("mongodb://localhost:27017/shopdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //promisObject resolved
+    console.log("Mongo Connection Open");
+  })
+  .catch((err) => {
+    //promisObject rejected
+    console.log("Mongo connection error has occured");
+    console.log(err);
+  });
+//Dont need to nest code inside callback - Operation Buffering
+//mongoose lets us use models immediately,without wainting
+//for mongoose to eastablish a connection to MongoDB
 
 //when view engine is used express assumes our views ie ejs templates
 //exist in a (default)views directory
 app.set("view engine", "ejs"); //auto require("ejs")
 //change path to absolute path to index.js
 app.set("views", path.join(__dirname, "/views"));
+
+//RESTful webApi crud operations pattern
+
+//httpMethod=get,path/resource-/test  -(direct match/exact path)
+//(READ) name-index,purpose-display all comments from DB server
+//convert (http structured) request to req jsObject + create res jsObject
+app.get("/test", (req, res) => {
+  res.send("test"); //content-type:text/plain + converted to httpStructued response
+});
 
 //adddress - localhost:3000
 //app is listening for (HTTPstructured) requests
