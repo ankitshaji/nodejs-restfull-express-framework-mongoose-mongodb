5ba1db60e1ed4ffe860c494b81574f741fb235a7
Ankit Shaji <ankitshaji@gmail.com>
2022-08-17 18:35:19 +1200

    npm i express,ejs,mongoose +initialize express app

---
+++ index.js
@@ -0,0 +1,17 @@
+//RESTful webApi - using REST principles
+const express = require("express"); //FunctionObject //express module
+const path = require("path"); //pathObject //path module
+const app = express(); //AppObject
+
+//when view engine is used express assumes our views ie ejs templates
+//exist in a (default)views directory
+app.set("view engine", "ejs"); //auto require("ejs")
+//change path to absolute path to index.js
+app.set("views", path.join(__dirname, "/views"));
+
+//adddress - localhost:3000
+//app is listening for (HTTPstructured) requests
+//executes callback
+app.listen(3000, () => {
+  console.log("listning on port 3000;");
+});
