51a664cc3182244e53dcbc91b862fe7262be90d1
Ankit Shaji <ankitshaji@gmail.com>
2022-08-19 03:27:59 +1200

    UPDATE-method(PUT),/res(/products/:id),name-update

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const app = express(); //AppObject
 //mongoose ODM - has callback but also supports promises-ie returns promiseObject (pending,undefined) to -resove(value)(fullfulled,value) or reject(errorMessage)(rejected,errorMessage)
 const mongoose = require("mongoose"); //mongooseObject //mongoose module
 const Product = require("./models/product"); //productClassObject(ie Model) //self created module/file needs "./"
+const methodOverride = require("method-override"); //functionObject //method-override module
 
-//form data middlewareMethod - http structured post request body parsed to req.body
-//http structure post request could be from browser form or postman
-app.use(express.urlencoded({ extended: true })); //app.use() execute when any httpMethod/any httpStructured request arrives
-
-// *******************************************
+// ********************************************************************************
 // CONNECT - nodeJS runtime app connects to default mogod server port + creates db
-// *******************************************
+// ********************************************************************************
 //mongooseObject.method(url/defaultPortNo/databaseToUse,optionsObject-notNeeded) //returns promiseObject pending
 mongoose
   .connect("mongodb://localhost:27017/farmStanddb", {
@@ -40,9 +37,17 @@ app.set("view engine", "ejs"); //auto require("ejs")
 //change path to absolute path to index.js
 app.set("views", path.join(__dirname, "/views"));
 
-// *********************************************************************************************************
-//RESTful webApi crud operations pattern + MongoDB CRUD Operations using mongoose-ODM (modelClassObject)
-// *********************************************************************************************************
+//middlewareFunctionObject() - override req.method from eg.POST to value of _method key eg.PUT,PATCH
+//?queryString - (?key=value) therefore _method is key, we set value to it in html form
+app.use(methodOverride("_method")); //app.use() executes when any httpMethod/any httpStructured request arrives
+
+//Accept form data - AppObject.middlewareMethod() - (http structured) POST request body parsed to req.body
+//(http structure) POST request could be from browser form or postman
+app.use(express.urlencoded({ extended: true })); //app.use() executes when any httpMethod/any httpStructured request arrives
+
+// *********************************************************************************************************************************************************
+//RESTful webApi crud operations pattern (route/pattern matching algorithm - order matters) + MongoDB CRUD Operations using mongoose-ODM (modelClassObject)
+// *********************************************************************************************************************************************************
 
 //httpMethod=GET,path/resource-/products  -(direct match/exact path)
 //(READ) name-index,purpose-display all documents in (products)collection from (farmStanddb)db
@@ -51,9 +56,9 @@ app.set("views", path.join(__dirname, "/views"));
 //async(ie continues running outside code if it hits an await inside) callback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
 //async function expression without an await is just a normal syncronous function expression
 app.get("/products", async (req, res) => {
-  // *******************************************
+  // *****************************************************
   //READ - querying a collection for a document/documents
-  // *******************************************
+  // *****************************************************
   //productClassObject.method(queryObject) ie modelClassObject.method() - same as - db.products.find({})
   //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
   const products = await Product.find({}); //products = dataObject ie array of all jsObjects(documents)
@@ -62,7 +67,7 @@ app.get("/products", async (req, res) => {
   //render() - converts and sends res jsObject as (http structure)response //content-type:text/html
 });
 
-//(http stuctured) get request to form path - (http structured) response is pure html converted from form ejs file
+//(http stuctured) GET request to form path - (http structured) response is pure html converted from form ejs file
 //httpMethod=GET,path/resource-/products/new  -(direct match/exact path)
 //(READ) name-new,purpose-display form to submit new document into (products)collection of (farmStanddb)db
 //execute callback when http structure request arrives
@@ -81,9 +86,9 @@ app.get("/products/new", (req, res) => {
 //async(ie continues running outside code if it hits an await inside) callback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
 //async function expression without an await is just a normal syncronous function expression
 app.post("/products", async (req, res) => {
-  // *******************************************
+  // ***************************************************************************************
   //CREATE - creating a single new document in the (products) collection of (farmStanddb)db
-  // *******************************************
+  // ***************************************************************************************
   //modelClass
   //productClassObject(objectArgument-passed to constructor method)
   //objectArgument- jsObject{key:value} ie the new document that abides to collectionSchemaInstanceObject
@@ -114,15 +119,69 @@ app.get("/products/:id", async (req, res) => {
   //could use productName if it was webSlug(url safe)
   //object keys to variable - Object destructuring
   const { id } = req.params; //pathVariablesObject
-  // *******************************************
+  // *************************************************
   //READ - querying a collection for a document by id
-  // *******************************************
+  // *************************************************
   //productClassObject.method(idString) ie modelClassObject.method() - same as - db.products.findOne({_id:"12345"})
   //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
   const product = await Product.findById(id); //product = dataObject ie single first matching jsObject(document)
   res.render("products/show", { product: product }); //(ejs filePath,variable sent to ejs)
   //render() - executes js - converts  ejs file into pure html
   //render() - converts and sends res jsObject as (http structure)response //content-type:text/html
+});
+
+//(http stuctured) GET request to form path - (http structured) response is pure html converted from form ejs file
+//httpMethod=GET,path/resource-/products/:id/edit  -(pattern match) //:id is a path variable
+//(READ) name-edit,purpose-display form to edit existing document in (products)collection of (farmStanddb)db
+//execute callback when http structure request arrives
+//convert (http structured) request to req jsObject + create res jsObject
+//async(ie continues running outside code if it hits an await inside) callback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
+//async function expression without an await is just a normal syncronous function expression
+app.get("/products/:id?/edit", async (req, res) => {
+  //could use productName if it was webSlug(url safe)
+  //object keys to variable - Object destructuring
+  const { id } = req.params; //pathVariablesObject
+  // ***********************************************************
+  //READ - querying a collection(products) for a document by id
+  // ***********************************************************
+  //productClassObject.method(idString) ie modelClassObject.method() - same as - db.products.findOne({_id:"12345"})
+  //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
+  const foundProduct = await Product.findById(id); //foundProduct = dataObject ie single first matching jsObject(document)
+  //passing in foundProduct to prepoppulate form
+  res.render("products/edit", { product: foundProduct }); //(ejs filePath,renamed variable sent to ejs)
+  //render() - executes js - converts  ejs file into pure html
+  //render() - converts and sends res jsObject as (http structure)response //content-type:text/html
+});
+
+//httpMethod=PUT,path/resource-/products/:id  -(pattern match) //:id is a path variable
+//(UPDATE) name-update,purpose-completely replace/update single specific existing document in (products)collection of (farmStanddb)db
+//execute callback when (http structure) request arrives
+//convert (http structured) request to req jsObject + create res jsObject
+//(http structured) request body contains data - middleware parses to req.body
+//async(ie continues running outside code if it hits an await inside) callback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
+//async function expression without an await is just a normal syncronous function expression
+app.put("/products/:id", async (req, res) => {
+  //object keys to variable - Object destructuring
+  const { id } = req.params; //pathVariablesObject
+  //form data ie req.body is jsObject {key/name:inputValue,key/name:inputValue,key/name:inputValue}
+  // **************************************************************************************************************
+  //UPDATE - querying a collection(products) for a document by id then updating it + new key:value pairs neglected
+  // **************************************************************************************************************
+  //modelClass
+  //productClassObject.method(idString,updateObject,optionObject) ie modelClassObject.method() - same as - db.products.findOneAndUpdate(({_id:"12345"},{$set:{name:"x",...}},{returnNewDocument:true})
+  //returns thenableObject - pending to resolved(dataObject),rejected(errorObject) ie(breaking validation/contraints)
+  //To run validations/contraints when updating we need to set runValidators(key) in optionsObject
+  //To get the jsObject(document) after update, we need to set new(key) in optionsObject
+  //queries (products)collection of (farmStanddb)db for single document by idString and updates/replaces the document with new updateObject(document)
+  const foundProduct = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  }); //foundProduct = dataObject ie single first matching jsObject(document) after it was updated
+  //fix for page refresh sending duplicate (http structured) PUT request -
+  res.redirect(`/products/${foundProduct._id}`);
+  //console.dir(res._header); //res.statusCode set to 302-found ie redirect //res.location set to /products/:id
+  //converts and sends res jsObject as (http structure)response //default content-type:text/html
+  //browser sees (http structured) response with headers and makes a (http structured) get request to location ie default(get)/products/:id
 });
 
 //adddress - localhost:3000
